2a473d478cbabd1848dce3093b3f292503c3f6c8
Luna <jhaught2007@gmail.com>
2026-02-16 00:36:24 -0500

    dumb these things ()

--- modules/functions/ip-cache.mjs
+++ modules/functions/ip-cache.mjs
@@ -29,7 +29,7 @@ export async function setCache(identifier, type, data) {
         VALUES (?, ?, ?, ?)
         ON DUPLICATE KEY UPDATE
             data = VALUES(data),
-            created = NOW
+            created = NOW()
         `,
         [identifier, type, data]
     );
